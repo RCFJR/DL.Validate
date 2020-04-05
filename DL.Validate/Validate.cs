b4be5c402793ea8c6a182b9bce9d6fca7fbde2d8
SmartRequest <43654675+SmartRequest@users.noreply.github.com>
2020-04-04 21:41:13 -0300

    primeiro commit da primeira versão

---
+++ DL.Validate/Validate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL.Validate
+{
+    public class Validate
+    {
+        public static bool Verify<T>(T entity)
+        {
+            Type Object = typeof(T);
+            PropertyInfo[] props = Object.GetProperties();
+            object valueOf = null;
+
+            foreach (PropertyInfo prop in props)
+            {
+                var req = prop.CustomAttributes;
+                if (req.Any(e => e.AttributeType.Name.Equals("RequiredAttribute")))
+                {
+                    valueOf = prop.GetValue(entity, null);
+                    if (String.IsNullOrEmpty(valueOf.ToString()))
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
